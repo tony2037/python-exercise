c8dc72fbff34caa7c80c57e9cd591e37299b004b
tony2037 <ztex030640417@gmail.com>
2017-08-18 00:21:10 +0800

    am Tired

--- exercise1.py
+++ exercise1.py
@@ -4,8 +4,34 @@ def translate(arr):
         return_arr.append(str(tem)+'t')
     return return_arr
 
-temperatures=[10,-20,-289,100]
-temperatures=translate(temperatures)
+def something():
+    temperatures=[10,-20,-289,100]
+    temperatures=translate(temperatures)
 
-for tem in temperatures:
-    print(tem)
+    for tem in temperatures:
+        print(tem)
+
+class test:
+    def __init__(self,value=0,commit=''):
+        self.value = value
+        self.commit = commit
+    def get_value(self):
+        return self.value
+    def set_value(v):
+        self.value = v
+    def get_commit(self):
+        return self.commit
+    def set_commit(c):
+        self.commit = c
+
+if __name__=='__main__':
+    tl = [test(),test(commit='Dude'),test(value=87),test(17,'Love')]
+    for t_i in tl:
+        print(t_i.value)
+        print(t_i.commit)
+        #print(type(t_i.get_value()))
+        #print(t_i.get_value())
+        #print(t_i.get_commit())
+    for t_i in tl:
+        print(t_i.get_value())
+        print(t_i.get_commit())
